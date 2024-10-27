32f4d34899a5999d8bd0ca84af354d744872c4f0
HAHOOS <hahoos@hahoos.pl>
2024-10-27 20:49:40 +0100

    Revamp the DevMode & WebSocket system

--- BONELABDevTools/DevMode.cs
+++ BONELABDevTools/DevMode.cs
@@ -1,88 +1,214 @@
-﻿using static BonelabDevMode.Form1;
+﻿using BonelabDevMode.JSON;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using Newtonsoft.Json.Schema;
+using static BonelabDevMode.Form1;
 
 namespace BonelabDevMode
 {
     internal class DevMode
     {
-        private static WebSocketSharp.WebSocket websocket;
+        private static WebSocketSharp.WebSocket? websocket;
         public static WebSocketConnectionState state;
         public static string path = "ws://127.0.0.1:50152/console";
-        public string command = "";
-        public static bool DevModeEnabled;
+
+        internal static WebSocketEvents? events;
+
+        private static string lastCommand = string.Empty;
+        private static long? lastCommand_ExecutionDate;
+
+        public static List<int> DontLog = [];
+
+        public static void UpdateCoordinates()
+        {
+            var timer = new System.Timers.Timer();
+            timer.Elapsed += (s, e) =>
+            {
+                if (websocket == null || websocket.ReadyState == WebSocketSharp.WebSocketState.Closing || websocket.ReadyState == WebSocketSharp.WebSocketState.Closed || websocket.ReadyState == WebSocketSharp.WebSocketState.Connecting || events == null || state != WebSocketConnectionState.CONNECTED)
+                {
+                    timer.Stop();
+                    return;
+                }
+                // HACK: Couldn't in any other way use the _event variable inside the eventhandler action
+                EventHandler<CustomMessageEventArgs>? _event = null;
+                _event = (sender, msg) =>
+                {
+                    if (msg.EventArgs != null && msg.EventArgs.Data != null && msg.EventArgs.Data.StartsWith("whereami: teleport"))
+                    {
+                        var coordinates = msg.EventArgs.Data.Replace("whereami: teleport ", string.Empty);
+                        DontLog.Add(msg.MessageID);
+                        Form1.Instance?.UpdateCoordinates(coordinates, ref events, ref _event);
+                    }
+                };
+                events.OnMessage += _event;
+                websocket.Send("whereami");
+            };
+            timer.Interval = 100;
+            timer.AutoReset = true;
+            timer.Start();
+        }
+
+        public static void UpdateCurrentLevel()
+        {
+            var timer = new System.Timers.Timer();
+            timer.Elapsed += (s, e) =>
+            {
+                if (websocket == null || websocket.ReadyState == WebSocketSharp.WebSocketState.Closing || websocket.ReadyState == WebSocketSharp.WebSocketState.Closed || websocket.ReadyState == WebSocketSharp.WebSocketState.Connecting || events == null || state != WebSocketConnectionState.CONNECTED)
+                {
+                    timer.Stop();
+                    return;
+                }
+
+                // HACK: Couldn't in any other way use the _event variable inside the eventhandler action
+                EventHandler<CustomMessageEventArgs>? _event = null;
+                bool awaitNext = false;
+                _event = (sender, msg) =>
+                {
+                    if (msg.EventArgs.Data == "Active Scene") awaitNext = true;
+                    else
+                    {
+                        if (IsSceneSchemaValid(msg.EventArgs.Data) && awaitNext)
+                        {
+                            Scene? scene = JsonConvert.DeserializeObject<Scene>(msg.EventArgs.Data);
+                            if (scene != null)
+                            {
+                                Form1.Instance?.UpdateCurrentLevel(scene.name, ref events, ref _event);
+                            }
+                        }
+                    }
+                };
+
+                events.OnMessage += _event;
+                websocket.Send("scene.list");
+            };
+            timer.Interval = 2500;
+            timer.AutoReset = true;
+            timer.Start();
+        }
 
         public static void ConnectButton()
         {
-            Form1.Instance.SetEnabledState(Form1.WebSocketConnectionState.CONNECTING);
+            Form1.Instance?.SetEnabledState(Form1.WebSocketConnectionState.CONNECTING);
             state = WebSocketConnectionState.CONNECTING;
             if (websocket != null)
             {
-                Form1.Instance.AddLog("An existing connection was found, disconnecting");
+                Form1.Instance?.AddLog("An existing connection was found, disconnecting");
                 ((IDisposable)websocket).Dispose();
                 websocket = null;
             }
 
             try
             {
-                Form1.Instance.AddLog("Connecting...");
+                Form1.Instance?.AddLog("Connecting...");
                 websocket = new WebSocketSharp.WebSocket(path);
-                websocket.OnMessage += (sender, e) => Form1.Instance.AddLog($"[WEBSOCKET] {e.Data.Clone()}");
+                events = new WebSocketEvents(websocket);
+                events.OnLateMessage += (sender, e) => { if (!DontLog.Contains(e.MessageID)) Form1.Instance?.AddLog($"[WEBSOCKET] {e.EventArgs.Data}"); };
+                events.OnMessage += UpdateCurrentLevel_HideMessages;
 
                 websocket.OnOpen += (sender, e) =>
                 {
-                    Form1.Instance.SetEnabledState(Form1.WebSocketConnectionState.CONNECTED);
+                    Form1.Instance?.SetEnabledState(Form1.WebSocketConnectionState.CONNECTED);
                     state = WebSocketConnectionState.CONNECTED;
-                    Form1.Instance.AddLog($"[WEBSOCKET] Successfully connected!");
+                    Form1.Instance?.AddLog($"[WEBSOCKET] Successfully connected!");
+                    UpdateCurrentLevel();
+                    UpdateCoordinates();
                 };
-                websocket.OnError += (sender, e) => Form1.Instance.AddLog($"[ERROR] [WEBSOCKET] The WebSocket threw an exception: {e}");
+                websocket.OnError += (sender, e) => Form1.Instance?.AddLog($"[ERROR] [WEBSOCKET] The WebSocket threw an exception: {e}", LogType.ERROR);
                 websocket.OnClose += (sender, e) =>
                 {
-                    Form1.Instance.SetEnabledState(Form1.WebSocketConnectionState.DISCONNECTED);
+                    Form1.Instance?.SetEnabledState(Form1.WebSocketConnectionState.DISCONNECTED);
                     state = WebSocketConnectionState.DISCONNECTED;
-                    Form1.Instance.AddLog($"[WEBSOCKET] WebSocket closed/disconnected");
+                    Form1.Instance?.AddLog($"[WEBSOCKET] WebSocket closed/disconnected");
                 };
 
                 websocket.Connect();
             }
             catch (Exception e)
             {
-                Form1.Instance.AddLog($"[ERROR] [WEBSOCKET] {e}");
-                Form1.Instance.SetEnabledState(WebSocketConnectionState.DISCONNECTED);
+                Form1.Instance?.AddLog($"[ERROR] [WEBSOCKET] {e}");
+                Form1.Instance?.SetEnabledState(Form1.WebSocketConnectionState.DISCONNECTED);
                 state = WebSocketConnectionState.DISCONNECTED;
             }
+        }
+
+        internal static void AddProperty(ref JSchema schema, string name, JSchemaType type)
+        {
+            JSchema jSchema = new()
+            {
+                Type = type
+            };
+            schema.Properties.Add(name, jSchema);
+            schema.Required.Add(name);
+        }
+
+        internal static bool IsSceneSchemaValid(string message)
+        {
+            JSchema schema = new()
+            {
+                Type = JSchemaType.Object
+            };
+
+            AddProperty(ref schema, "buildIndex", JSchemaType.Integer);
+            AddProperty(ref schema, "isDirty", JSchemaType.Boolean);
+            AddProperty(ref schema, "isLoaded", JSchemaType.Boolean);
+            AddProperty(ref schema, "name", JSchemaType.String);
+            AddProperty(ref schema, "path", JSchemaType.String);
+            AddProperty(ref schema, "rootCount", JSchemaType.Integer);
+            try
+            {
+                JObject json = JObject.Parse(message);
+                return json.IsValid(schema);
+            }
+            catch (JsonReaderException)
+            {
+                return false;
+            }
+        }
+
+        private static void UpdateCurrentLevel_HideMessages(object? sender, CustomMessageEventArgs e)
+        {
+            if ((e.EventArgs.Data.StartsWith("Scene") || e.EventArgs.Data.StartsWith("Active Scene")) && (!lastCommand.StartsWith("scene.list") || (lastCommand_ExecutionDate == null || DateTimeOffset.Now.ToUnixTimeMilliseconds() - lastCommand_ExecutionDate >= 100))) DontLog.Add(e.MessageID);
+            else if (IsSceneSchemaValid(e.EventArgs.Data) && (!lastCommand.StartsWith("scene.list") || (lastCommand_ExecutionDate == null || DateTimeOffset.Now.ToUnixTimeMilliseconds() - lastCommand_ExecutionDate >= 100))) DontLog.Add(e.MessageID);
         }
 
         public static void DisconnectButton()
         {
             try
             {
-                Form1.Instance.AddLog("Disconnecting...");
+                Form1.Instance?.AddLog("Disconnecting...");
                 state = WebSocketConnectionState.DISCONNECTING;
-                Form1.Instance.SetEnabledState(WebSocketConnectionState.DISCONNECTING);
-                websocket.Close();
-                ((IDisposable)websocket).Dispose();
+                Form1.Instance?.SetEnabledState(WebSocketConnectionState.DISCONNECTING);
+                websocket?.Close();
+                (websocket as IDisposable)?.Dispose();
                 websocket = null;
             }
             catch (Exception e)
             {
-                Form1.Instance.AddLog($"[ERROR] [WEBSOCKET] {e}");
-                Form1.Instance.SetEnabledState(WebSocketConnectionState.DISCONNECTED);
+                Form1.Instance?.AddLog($"[ERROR] [WEBSOCKET] {e}");
+                Form1.Instance?.SetEnabledState(WebSocketConnectionState.DISCONNECTED);
                 state = WebSocketConnectionState.DISCONNECTED;
             }
         }
 
         public static void ReloadLevel()
         {
-            websocket.Send($"level.reload");
+            lastCommand = "level.reload";
+            lastCommand_ExecutionDate = DateTimeOffset.Now.ToUnixTimeMilliseconds();
+            websocket?.Send($"level.reload");
         }
 
         public static void ReloadPallet(string palletBarcode)
         {
-            websocket.Send($"assetwarehouse.reload {palletBarcode}");
+            lastCommand = "assetwarehouse.reload " + palletBarcode;
+            lastCommand_ExecutionDate = DateTimeOffset.Now.ToUnixTimeMilliseconds();
+            websocket?.Send($"assetwarehouse.reload {palletBarcode}");
         }
 
         public static void SendCommand(string command)
         {
-            websocket.Send(command);
+            lastCommand = command;
+            lastCommand_ExecutionDate = DateTimeOffset.Now.ToUnixTimeMilliseconds();
+            websocket?.Send(command);
         }
     }
 }
---
+++ BONELABDevTools/WebSocketEvents.cs
@@ -0,0 +1,37 @@
+﻿using WebSocketSharp;
+
+namespace BonelabDevMode
+{
+    internal class WebSocketEvents
+    {
+        internal WebSocket webSocket;
+
+        public int MessageID = 0;
+
+        public event EventHandler<CustomMessageEventArgs> OnLateMessage;
+
+        public event EventHandler<CustomMessageEventArgs> OnMessage;
+
+#pragma warning disable CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider adding the 'required' modifier or declaring as nullable.
+
+        public WebSocketEvents(WebSocket webSocket)
+#pragma warning restore CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider adding the 'required' modifier or declaring as nullable.
+        {
+            this.webSocket = webSocket;
+            webSocket.OnMessage += (sender, e) =>
+            {
+                MessageID++;
+                var args = new CustomMessageEventArgs(e, MessageID);
+                OnMessage?.Invoke(this, args);
+                OnLateMessage?.Invoke(this, args);
+            };
+        }
+    }
+
+    public class CustomMessageEventArgs(MessageEventArgs args, int messageID) : EventArgs
+    {
+        public int MessageID { get; private set; } = messageID;
+
+        public MessageEventArgs EventArgs { get; private set; } = args;
+    }
+}
