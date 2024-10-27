c34e410dc91b4251fec5c509c5481a0a9069812e
HAHOOS <hahoos@hahoos.pl>
2024-10-27 12:20:29 +0100

    Add support for logs and connection state label

--- BONELABDevTools/DevMode.cs
+++ BONELABDevTools/DevMode.cs
@@ -1,38 +1,53 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Diagnostics;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows.Forms;
-using WebSocketSharp;
+﻿using static BonelabDevMode.Form1;
 
 namespace BonelabDevMode
 {
     internal class DevMode
     {
         private static WebSocketSharp.WebSocket websocket;
+        public static WebSocketConnectionState state;
         public static string path = "ws://127.0.0.1:50152/console";
         public string command = "";
         public static bool DevModeEnabled;
 
         public static void ConnectButton()
         {
+            Form1.Instance.SetEnabledState(Form1.WebSocketConnectionState.CONNECTING);
+            state = WebSocketConnectionState.CONNECTING;
             if (websocket != null)
             {
+                Form1.Instance.AddLog("An existing connection was found, disconnecting");
                 ((IDisposable)websocket).Dispose();
                 websocket = null;
             }
 
             try
             {
+                Form1.Instance.AddLog("Connecting...");
                 websocket = new WebSocketSharp.WebSocket(path);
-                websocket.OnMessage += (sender, e) => Console.WriteLine(e.Data);
+                websocket.OnMessage += (sender, e) => Form1.Instance.AddLog($"[WEBSOCKET] {e.Data.Clone()}");
+
+                websocket.OnOpen += (sender, e) =>
+                {
+                    Form1.Instance.SetEnabledState(Form1.WebSocketConnectionState.CONNECTED);
+                    state = WebSocketConnectionState.CONNECTED;
+                    Form1.Instance.AddLog($"[WEBSOCKET] Successfully connected!");
+                };
+                websocket.OnError += (sender, e) => Form1.Instance.AddLog($"[ERROR] [WEBSOCKET] The WebSocket threw an exception: {e}");
+                websocket.OnClose += (sender, e) =>
+                {
+                    Form1.Instance.SetEnabledState(Form1.WebSocketConnectionState.DISCONNECTED);
+                    state = WebSocketConnectionState.DISCONNECTED;
+                    Form1.Instance.AddLog($"[WEBSOCKET] WebSocket closed/disconnected");
+                };
+
                 websocket.Connect();
             }
             catch (Exception e)
             {
-                Console.WriteLine(e);
+                Form1.Instance.AddLog($"[ERROR] [WEBSOCKET] {e}");
+                Form1.Instance.SetEnabledState(WebSocketConnectionState.DISCONNECTED);
+                state = WebSocketConnectionState.DISCONNECTED;
             }
         }
 
@@ -40,13 +55,18 @@ namespace BonelabDevMode
         {
             try
             {
+                Form1.Instance.AddLog("Disconnecting...");
+                state = WebSocketConnectionState.DISCONNECTING;
+                Form1.Instance.SetEnabledState(WebSocketConnectionState.DISCONNECTING);
                 websocket.Close();
                 ((IDisposable)websocket).Dispose();
                 websocket = null;
             }
             catch (Exception e)
             {
-                Console.WriteLine(e);
+                Form1.Instance.AddLog($"[ERROR] [WEBSOCKET] {e}");
+                Form1.Instance.SetEnabledState(WebSocketConnectionState.DISCONNECTED);
+                state = WebSocketConnectionState.DISCONNECTED;
             }
         }
 
