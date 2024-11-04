f77eeda0465232027d4e563a9663846681ffe80b
HAHOOS <hahoos@hahoos.pl>
2024-11-04 16:01:31 +0100

    Remove unnecessary code and do some small changes

--- BONELABDevTools/CustomBackgroundWorker.cs
+++ BONELABDevTools/CustomBackgroundWorker.cs
@@ -1,48 +1,18 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Diagnostics;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace BonelabDevMode
+﻿namespace BonelabDevMode
 {
-    public class Worker
-    {
-        // This method will be called when the thread is started.
-        public void DoWork()
-        {
-            while (!_shouldStop)
-            {
-                Console.WriteLine("worker thread: working...");
-            }
-            Console.WriteLine("worker thread: terminating gracefully.");
-        }
-
-        public void RequestStop()
-        {
-            _shouldStop = true;
-        }
-
-        // Volatile is used as hint to the compiler that this data
-        // member will be accessed by multiple threads.
-        private volatile bool _shouldStop;
-    }
-
     internal class CustomBackgroundWorker
     {
-        public Action Start;
+        public Action? Start;
 
-        public event Action Finish;
+        public event Action? Finish;
 
-        public Thread? CurrentThread;
+        public Thread? CurrentThread { get; private set; }
 
         public void Work()
         {
             CurrentThread = new Thread(() =>
             {
-                Start();
+                Start?.Invoke();
                 Finish?.Invoke();
             })
             {
